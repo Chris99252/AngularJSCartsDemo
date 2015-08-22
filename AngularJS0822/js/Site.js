fb46bb83a4e03600d41243ef19dbbadc2ffd3424
Chris99252 <chris@ccf.org.tw>
2015-08-22 23:40:53 +0800

    07 加入購買 10 件以上自動打九折的商業邏輯到控制器中

--- AngularJS0822/js/Site.js
+++ AngularJS0822/js/Site.js
@@ -3,11 +3,20 @@ function MainCtrl($scope) {
 
     //$scope.PName = "T-shirt";
     //$scope.Qty = 1;
-    //$scope.Price = 299;
+    //$scope.Price = 100;
 
     // ViewModel
     var vm = this;
     vm.PName = "T-Shrit";
     vm.Qty = 1;
-    vm.Price = 1000;
+    vm.Price = 100;
+
+    // 10 件以上自動打九折的商業邏輯
+    vm.subTotal = function () {
+        var total = vm.Qty * vm.Price;
+        if (vm.Qty >= 10) {
+            total = total * 9;
+        }
+        return total;
+    }
 }
