ec1ab680ef17921c0ec0b5aab4eece5efac0a214
Chris99252 <chris@ccf.org.tw>
2015-08-22 23:36:57 +0800

    06 改用 ViewModel 的方式存取 Model ( View 使用 ControllerAs 語法 )

--- AngularJS0822/js/Site.js
+++ AngularJS0822/js/Site.js
@@ -1,5 +1,13 @@
-function MainCtrl ($scope) {
-    $scope.PName = "T-shirt";
-    $scope.Qty = 1;
-    $scope.Price = 299;
+function MainCtrl($scope) {
+    // GlobalModel
+
+    //$scope.PName = "T-shirt";
+    //$scope.Qty = 1;
+    //$scope.Price = 299;
+
+    // ViewModel
+    var vm = this;
+    vm.PName = "T-Shrit";
+    vm.Qty = 1;
+    vm.Price = 1000;
 }
